6d0ae96e878d2718d0e1e27f495efcf13d8b4eb6
LBarros360 <lbarros360@gmail.com>
2025-12-23 07:54:15 -0300

    AtPreparacao MySQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,18 @@
 CREATE TABLE usuarios (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255),
-    email VARCHAR(255),
-    login VARCHAR(50),
-    SENHA VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    login VARCHAR(50) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     logradouro VARCHAR(255),
-    numero INTEGER,
+    numero INT,
     bairro VARCHAR(50),
     cidade VARCHAR(50),
-    uf VARCHAR(50),
-    cep VARCHAR(8),
-    tipo_usuario VARCHAR(50),
-    data_alteracao TIMESTAMP
+    uf CHAR(2),
+    cep CHAR(8),
+    tipo_usuario VARCHAR(50) NOT NULL,
+    data_alteracao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
+    CONSTRAINT uk_usuario_email UNIQUE (email),
+    CONSTRAINT uk_usuario_login UNIQUE (login)
 );
-
-INSERT INTO usuarios (nome, email, login, senha, logradouro, numero, bairro, cidade, uf, cep, data_alteracao, tipo_usuario) VALUES
-    ('João Silva', 'joao_silva@gmail.com', 'joao01', 'pass0101', 'Rua XV', '500', 'Centro', 'Porto Alegre', 'RS', '80220335','2025-10-15 10:00:00', 'CLIENTE' );
-
-INSERT INTO usuarios (nome, email, login, senha, logradouro, numero, bairro, cidade, uf, cep, data_alteracao, tipo_usuario) VALUES
-    ('Maria dos Santos', 'maria_santos@gmail.com', 'maria01', 'pass0202', 'Rua XV', '500', 'Centro', 'Curitiba', 'PR', '80550335', '2025-10-15 10:00:00', 'DONO' );
-
-INSERT INTO usuarios (nome, email, login, senha, logradouro, numero, bairro, cidade, uf, cep, data_alteracao, tipo_usuario) VALUES
-    ('Ana Dantas', 'ana_dantas@gmail.com', 'ana01', 'pass0303', 'Rua XV', '500', 'Centro', 'Florianopolis', 'SC', '80234335', '2025-10-15 10:00:00', 'CLIENTE' );
-
-INSERT INTO usuarios (nome, email, login, senha, logradouro, numero, bairro, cidade, uf, cep, data_alteracao, tipo_usuario) VALUES
-    ('José Batista', 'jose_batista@gmail.com', 'jose01', 'pass0404', 'Rua XV', '500', 'Centro', 'São Paulo', 'SP', '80220785', '2025-10-15 10:00:00', 'CLIENTE' );
-
-INSERT INTO usuarios (nome, email, login, senha, logradouro, numero, bairro, cidade, uf, cep, data_alteracao, tipo_usuario) VALUES
-    ('Roberto Oliveiro', 'roberto_oliveira@gmail.com', 'roberto01', 'pass0505', 'Rua XV', '500', 'Centro', 'Belo Horizonte', 'MG', '80220455', '2025-10-15 10:00:00', 'CLIENTE' );
